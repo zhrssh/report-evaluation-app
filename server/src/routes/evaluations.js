b81435e068569263c9c4e3e98bc42969a46caa65
zhrssh <98146850+zhrssh@users.noreply.github.com>
2023-10-03 17:42:01 +0800

    Added CRUD functionality on evaluations route

--- server/src/routes/evaluations.js
+++ server/src/routes/evaluations.js
@@ -1,21 +1,55 @@
-import { create, readAll } from "../controller/evaluationController.js";
+import {
+	read,
+	create,
+	fetchIds,
+	update,
+	deleteOne,
+} from "../controller/evaluationController.js";
 
 export default function Route(fastify, opts, done) {
 	/**
 	 * Creates an evaluation entry to the database
 	 */
-	fastify.post("/create", async function (request, reply) {
+	fastify.post("/", async function (request, reply) {
 		const data = request.body;
-		await create(data);
-		reply.send({ msg: "OK" });
+		const _id = await create(data);
+		reply.send({ id: _id });
 	});
 
 	/**
 	 * Gets all evaluation entries from the database
 	 */
 	fastify.get("/", async function (request, reply) {
-		const results = await readAll();
-		reply.send(results);
+		const ids = await fetchIds();
+		reply.send({ ids });
+	});
+
+	/**
+	 * Retrieve a single entry from the database
+	 */
+	fastify.get("/view/:uid", async function (request, reply) {
+		const { uid } = request.params;
+		const result = await read({ _id: uid });
+		reply.send(result);
+	});
+
+	/**
+	 * Update an evaluation entry in the database
+	 */
+	fastify.put("/update/:uid", async function (request, reply) {
+		const { uid } = request.params;
+		const data = request.body;
+		const result = await update({ _id: uid }, data);
+		reply.send(result);
+	});
+
+	/**
+	 * Delete an entry from the database
+	 */
+	fastify.delete("/delete/:uid", async function (request, reply) {
+		const { uid } = request.params;
+		if (await deleteOne(uid)) reply.send(200);
+		else reply.code(400);
 	});
 
 	done();
